f612639142dc0ed51c3da3b95b018e154985fb2b
Maria Fernanda <mafenandaup@gmail.com>
2024-12-04 18:01:04 -0300

    process-imports

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,10 @@
 # aqui será feito o pré-processamento das palavras para a filtração apropriada.
+
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
